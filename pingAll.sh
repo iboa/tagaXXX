aef596363c33e187a1c03568fdae7b32b12a97ec
Darrin Meek <darrinmeek@aolcom>
2016-03-04 16:40:19 -0500

    2016 updates

--- pingAll.sh
+++ pingAll.sh
@@ -6,10 +6,14 @@
 TAGA_DIR=~/scripts/taga
 source $TAGA_DIR/config
 
-PING_COUNT=10
+#PING_COUNT=10
+PING_COUNT=2
 SLEEP_TIME=1
 
-MYGATEWAY=10.0.0.1
+MYGATEWAY=$NETADDRPART.1
+MYGATEWAY=`route | grep default | cut -c17-30`
+MYGATEWAY=`route | grep default | cut -c16-30`
+echo; echo GATEWAY: $MYGATEWAY
 
 while true
 do
