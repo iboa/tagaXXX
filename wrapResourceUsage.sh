dd0ae7bebe77b76d52e1964e75e1ed62fd18067a
Darrin Meek <darrinmeek@aolcom>
2016-03-04 18:27:52 -0500

    2016 updates

--- wrapResourceUsage.sh
+++ wrapResourceUsage.sh
@@ -10,7 +10,7 @@ COMMAND_TO_WRAP=$TAGA_DIR/resourceUsage.sh
 
 for target in $targetList
 do
-  echo processing $target .......
+  echo $0 processing $target .......
   sleep 1 
   ssh -l $MYLOGIN_ID $target $COMMAND_TO_WRAP
   sleep 1 
