76d9f328d1f84495015eee918c0452787f572cc8
Darrin Meek <darrinmeek@aolcom>
2016-03-04 10:55:58 -0500

    2016 updates

--- iboa_backup.sh
+++ iboa_backup.sh
@@ -4,7 +4,7 @@
 #####################################################
 
 BACKUP_DATE=`date +%j%H%M%S`
-BACKUP_DIR=~/iboaBackup/iboaBackup_$BACKUP_DATE
+BACKUP_DIR=~/.iboaBackup/iboaBackup_$BACKUP_DATE
 mkdir -p $BACKUP_DIR; cp ~/.bashrc* $BACKUP_DIR
 echo IBOA backup: all .bashrc* files written to $BACKUP_DIR
 
