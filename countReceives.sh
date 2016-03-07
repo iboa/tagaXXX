6f0f02355ac5b2cf8a392ca59e8a014ba81602c6
Darrin Meek <darrinmeek@aolcom>
2016-03-06 23:49:53 -0500

    2016 updates

--- countReceives.sh
+++ countReceives.sh
@@ -32,13 +32,34 @@ $TAGA_DIR/printReceiversHeader.sh RECEIVERS $iter $startTime $startDTG
 
 curcount="xxxx"
 
+# init the columns cumulative
+let column_cumulative_count=0
+let column_target_cumulative_count=0
+let i=0
+let j=0
 for target in $targetList
 do
+     # create dynamic vars to hold the column counts and init to 0
+     let j=$j+1
+     p_val=$j
+     active_id=$p_val
+     declare "flag_$active_id"=0
+done
+
+for target in $targetList
+do
+
+  # reset the per target cumulative
+  #let column_target_cumulative_count=0
 
   # build Row output
 
   # init the row cumulative
   let row_cumulative=0
+
+  # create the column containers
+  let i=$i+1
+  #let i.pt=$i
 
   # pad target name as necessary to have nice output
   tgtlen=`echo $target | awk '{print length($0)}'`
@@ -63,10 +84,19 @@ do
     row=$target\....... 
   fi
 
+  let j=0
+
   # get the received count for (to) this target
   for target2 in $targetList
 
   do
+
+     # create the inner loop column containers
+     let j=$j+1
+     #p_val=$j
+     #active_id=$p_val
+     #declare "flag_$active_id"=0
+     #let i.pt=$i
 
     if [ $target == $target2 ] ; then
       # skip self
@@ -101,6 +131,50 @@ do
 
       # add this count to the cumulative
       let row_cumulative=$row_cumulative+$curcount
+      #column_cumulative=$column_cumulative+$curcount
+
+      #echo 11111111111 : i:$i j:$j : $target  : $curcount
+      let column_cumulative_count=$column_cumulative_count+$curcount
+
+      #if [ $i -eq $j ]; then
+        let column_target_cumulative_count=$column_target_cumulative_count+curcount
+      #fi
+
+      # dlm temp find me
+      #p_val=$i
+
+      p_val=$j
+      active_id=$p_val
+#      declare "flag_$active_id"=0
+
+      #let myvalue=$column_cumulative_count
+
+      let myvalue=$column_target_cumulative_count
+
+       #echo declaring $j $myvalue
+
+#      declare "flag_$active_id"=$myvalue
+
+#      declare "flag_$active_id"=$myvalue
+
+      mytmpvar="flag_$active_id"
+     
+      #echo 1111
+      #echo "${!mytmpvar}"
+      #echo 222222
+      #declare "$mytmpvar"=$mytmpvar+200
+      #let othertmp="${!mytmpvar}"+200
+
+      let othertmp="${!mytmpvar}"+$curcount
+
+      #declare "$mytmpvar"="${!mytmpvar}"+200
+
+      declare "$mytmpvar"=$othertmp
+
+      #echo "${!mytmpvar}"
+      #echo 3333333
+       
+
 
       let mycount=$curcount
       if [ $mycount -lt 10 ] ; then
@@ -139,6 +213,80 @@ do
   echo $row
   echo $row >> $TAGA_DIR/counts.txt
   echo $row >> $TAGA_DIR/countsReceives.txt
+ 
+  # dlm temp find me
+  #p_val=$i
+  #active_id=$p_val
+  ##let myvalue=$column_cumulative_count
+  #let myvalue=$column_target_cumulative_count
+  #declare "flag_$active_id"=$myvalue
 
 done
+
+
+# Build the final (Totals) row
+#for target in $targetList
+#do
+#  # init the columns cumulative
+#  let i=$i+1
+#  column_cumulative=$column_cumulative" "
+#  column_cumulative=$column_cumulative" "$i.pt
+#
+#  v="flag_$active_id"
+#
+#done
+#
+
+
+let count=0
+for target in $targetList
+do
+  let count=$count+1
+done
+
+column_cumulative=""
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do 
+
+  v="flag_$i"
+
+  value=`echo "${!v}"`
+
+  let valuelen=`echo $value | awk '{print length($0)}'`
+
+  # pad it
+  if [ $valuelen -eq 3 ] ; then
+     value=0$value
+  elif [ $valuelen -eq 2 ] ; then
+     value=00$value
+  elif [ $valuelen -eq 1 ] ; then
+     value=000$value
+  else
+     echo nothing to pad >/dev/null
+  fi
+
+  #column_cumulative=$column_cumulative" "`echo "${!v}"`
+  column_cumulative=$column_cumulative" "$value
+
+  let count=$count-1
+  if [ $count -eq 0 ] ; then
+    break
+  fi
+
+done
+
+#column_cumulative=$column_cumulative"............................. "`echo "${!v}"`
+column_cumulative=$column_cumulative" ............................ "$column_cumulative_count
+
+# Print a space
+echo 
+echo >> $TAGA_DIR/counts.txt
+echo >> $TAGA_DIR/countsReceives.txt
+
+# Print the final (Totals) row
+row="Totals:..........  $column_cumulative"
+echo $row
+echo $row >> $TAGA_DIR/counts.txt
+echo $row >> $TAGA_DIR/countsReceives.txt
+
 echo
