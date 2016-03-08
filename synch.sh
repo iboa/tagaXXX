29c6e43569211a1083ea009e9881ccb148b15e45
Darrin Meek <darrinmeek@aolcom>
2016-03-08 17:02:27 -0500

    2016 updates

--- synch.sh
+++ synch.sh
@@ -3,7 +3,9 @@
 # All Rights Reserved
 #####################################################
 
-TAGA_DIR=/opt/iboa
+#TAGA_DIR=/opt/iboa
+
+TAGA_DIR=~/scripts/taga
 source $TAGA_DIR/config
 
 for target in $targetList
@@ -17,18 +19,23 @@ do
    sleep 1
 
    # build the source file string
-   SCP_SOURCE_STR="*.sh"
-   SCP_SOURCE_STR="$SCP_SOURCE_STR *.txt"  
-   SCP_SOURCE_STR="$SCP_SOURCE_STR host*"  
-   SCP_SOURCE_STR="$SCP_SOURCE_STR config"
-   SCP_SOURCE_STR="$SCP_SOURCE_STR *.template"
+   SCP_SOURCE_STR="*.sh"                       # add *.sh files
+   SCP_SOURCE_STR="$SCP_SOURCE_STR *.txt"      # add *.txt files 
+   SCP_SOURCE_STR="$SCP_SOURCE_STR host*"      # add host* files
+   SCP_SOURCE_STR="$SCP_SOURCE_STR config"     # add config files
+   SCP_SOURCE_STR="$SCP_SOURCE_STR *.template" # add template files
+   SCP_SOURCE_STR="$SCP_SOURCE_STR code"       # add code subdir
 
    # send the files to the destination
-   # ssh -l $MYLOGIN_ID $target "sudo mkdir $TAGA_DiR  2>/dev/null"
-   # ssh -l $MYLOGIN_ID $target "sudo chmod 777 $TAGA_DIR  2>/dev/null"
-   ssh -l $MYLOGIN_ID $target "sudo mkdir /opt/iboa  2>/dev/null"
-   ssh -l $MYLOGIN_ID $target "sudo chmod 777 /opt/iboa  2>/dev/null"
-   scp $SCP_SOURCE_STR $MYLOGIN_ID@$target:$TAGA_DIR <$TAGA_DIR/passwd.txt
+   # ssh -l $MYLOGIN_ID $target "sudo mkdir $TAGA_DiR         2>/dev/null"
+   # ssh -l $MYLOGIN_ID $target "sudo chmod 777 $TAGA_DIR     2>/dev/null"
+   #ssh -l $MYLOGIN_ID $target "sudo mkdir /opt/iboa          2>/dev/null"
+   #ssh -l $MYLOGIN_ID $target "sudo chmod 777 /opt/iboa      2>/dev/null"
+   #ssh -l $MYLOGIN_ID $target "sudo mkdir /opt/iboa/taga     2>/dev/null"
+   #ssh -l $MYLOGIN_ID $target "sudo chmod 777 /opt/iboa/taga 2>/dev/null"
+   ssh -l $MYLOGIN_ID $target "sudo mkdir -p $TAGA_DIR   2>/dev/null"
+   ssh -l $MYLOGIN_ID $target "sudo chmod 777 $TAGA_DIR  2>/dev/null"
+   scp -r $SCP_SOURCE_STR $MYLOGIN_ID@$target:$TAGA_DIR <$TAGA_DIR/passwd.txt
 
    # clean up old OBE scripts (run once per file in all environs)
    # but check the flag first
