c3b1b7b4e7ec582e9b94ee502da22dfd881787d1
Darrin Meek <darrinmeek@aolcom>
2016-03-12 19:17:20 -0500

    March 2016 updates

--- iboa_padded_echo.sh
+++ iboa_padded_echo.sh
@@ -6,12 +6,12 @@
 TAGA_DIR=~/scripts/taga
 source $TAGA_DIR/config
 
-PADVAR="."
 PADVAR=";"
 PADVAR=";"
 PADVAR=":"
-PADVAR="x"
 PADVAR=" "
+PADVAR="x"
+PADVAR="."
 
 if [ $# -ne 2 ]; then
     echo $0: Error, two params required!
