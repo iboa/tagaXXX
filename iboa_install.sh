713f6d6771bed2a78fc8607ea6b2fd12f33c9025
Darrin Meek IBOA <darrinmeek@aol.com>
2016-03-13 00:08:36 -0500

    Added files via upload

--- iboa_install.sh
+++ iboa_install.sh
@@ -107,11 +107,21 @@ else
    # create the insertAliasPrevious function for the 'install alias previous' (previous command) (iap) alias
    echo "function __insertAliasPrevious() {"                                     >> $IBOA_FILE
    echo "  if [ \$# -eq 1 ]; then"                                               >> $IBOA_FILE
-   echo "    CMD=\`history | tail -n 2 | head -n 1 | cut -c8-\`"                 >> $IBOA_FILE
-   echo "    echo alias \$1=\'\$CMD\' >> $IBOA_USER_FILE"                        >> $IBOA_FILE
+   echo "    which \$1 "                                                         >> $IBOA_FILE
    echo "    if [ \$? -eq 0 ]; then"                                             >> $IBOA_FILE
-   echo "      source $IBOA_USER_FILE"                                           >> $IBOA_FILE
-   echo "      echo alias\(es\) successfully installed into $IBOA_USER_FILE"     >> $IBOA_FILE
+   echo "      echo the \$1 command exists - no action taken!"                   >> $IBOA_FILE
+   echo "    else"                                                               >> $IBOA_FILE
+   echo "      alias \$1  >/dev/null "                                           >> $IBOA_FILE
+   echo "      if [ \$? -eq 0 ]; then"                                           >> $IBOA_FILE
+   echo "        echo the \$1 alias exists - no action taken!"                   >> $IBOA_FILE
+   echo "      else"                                                             >> $IBOA_FILE
+   echo "        CMD=\`history | tail -n 2 | head -n 1 | cut -c8-\`"             >> $IBOA_FILE
+   echo "        echo alias \$1=\'\$CMD\' >> $IBOA_USER_FILE"                    >> $IBOA_FILE
+   echo "        if [ \$? -eq 0 ]; then"                                         >> $IBOA_FILE
+   echo "          source $IBOA_USER_FILE"                                       >> $IBOA_FILE
+   echo "          echo alias\(es\) successfully installed into $IBOA_USER_FILE" >> $IBOA_FILE
+   echo "        fi"                                                             >> $IBOA_FILE
+   echo "      fi"                                                               >> $IBOA_FILE
    echo "    fi"                                                                 >> $IBOA_FILE
    echo "  else"                                                                 >> $IBOA_FILE
    echo "    echo this alias requires a single parameter - no action taken!"     >> $IBOA_FILE
@@ -119,17 +129,22 @@ else
    echo "}"                                                                      >> $IBOA_FILE
 
    # create the wrapAliasPrevious function for the 'wrap alias previous' (previous command) (wap) alias
-   echo "function __wrapAliasPrevious() {"                                       >> $IBOA_FILE
+   echo "function __wrapAliasPrevious() {"                                     >> $IBOA_FILE
    echo "  if [ \$# -eq 1 ]; then"                                               >> $IBOA_FILE
    echo "    which \$1 "                                                         >> $IBOA_FILE
    echo "    if [ \$? -eq 0 ]; then"                                             >> $IBOA_FILE
-   echo "      echo the $1 command exists - no action taken!"                    >> $IBOA_FILE
+   echo "      echo the \$1 command exists - no action taken!"                   >> $IBOA_FILE
    echo "    else"                                                               >> $IBOA_FILE
-   echo "      CMD=\`history | tail -n 2 | head -n 1 | cut -c8-\`"               >> $IBOA_FILE
-   echo "      echo \"alias \$1='while true; do echo; date; \$CMD ; sleep 1; done'\" >> $IBOA_USER_FILE"  >> $IBOA_FILE
+   echo "      alias \$1  >/dev/null "                                           >> $IBOA_FILE
    echo "      if [ \$? -eq 0 ]; then"                                           >> $IBOA_FILE
-   echo "        source $IBOA_USER_FILE"                                         >> $IBOA_FILE
-   echo "        echo alias\(es\) successfully installed into $IBOA_USER_FILE"   >> $IBOA_FILE
+   echo "        echo the \$1 alias exists - no action taken!"                   >> $IBOA_FILE
+   echo "      else"                                                             >> $IBOA_FILE
+   echo "        CMD=\`history | tail -n 2 | head -n 1 | cut -c8-\`"             >> $IBOA_FILE
+   echo "        echo \"alias \$1='while true; do echo; date; \$CMD ; sleep 1; done'\" >> $IBOA_USER_FILE" >> $IBOA_FILE
+   echo "        if [ \$? -eq 0 ]; then"                                         >> $IBOA_FILE
+   echo "          source $IBOA_USER_FILE"                                       >> $IBOA_FILE
+   echo "          echo alias\(es\) successfully installed into $IBOA_USER_FILE" >> $IBOA_FILE
+   echo "        fi"                                                             >> $IBOA_FILE
    echo "      fi"                                                               >> $IBOA_FILE
    echo "    fi"                                                                 >> $IBOA_FILE
    echo "  else"                                                                 >> $IBOA_FILE
